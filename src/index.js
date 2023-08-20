0dfa37e7a251bc335c14cdcac6e652df4edf32e4
vadim-ma <color.ui>
2023-08-20 07:15:15 +0300

    feat: function check is full implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    let i = 0;
+    let wasReducted = true;
+    while(str.length > 0 && wasReducted){
+        wasReducted = false;
+        while(str.length > 0 && i + 1 < str.length){
+            const curChar = str[i];
+            const nextChar = str[i + 1];
+            const pair = bracketsConfig.find(pair => pair[0] === curChar);
+            if(pair && nextChar === pair[1]){
+                str = str.substring(0, i) + str.substring(i+2);
+                if(i > 0){
+                    i--;
+                }
+                wasReducted = true;
+            }else{
+                i++;
+            }
+        }
+    }
+    return str.length === 0;
 }
